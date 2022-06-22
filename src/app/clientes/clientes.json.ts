78642d55ecd0622e43feb6ca8d532a5bfa98f08e
kleber loayza <kloayza23@yahoo.com>
2022-06-22 13:40:34 -0500

    project angular

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,6 @@
+import{Cliente} from './cliente';
+
+export const CLIENTES: Cliente[]=[
+  {id:1,nombre: 'Kleber',apellido:'Loayza',email:'kloayza22@icloud.com',createAt:'2017-12-11'},
+  {id:2,nombre: 'Abel',apellido:'Loayza',email:'abel06@icloud.com',createAt:'2018-12-11'}
+];
